8f841bdfb78f3cea387c6ede1b5b2b8431313cc5
Surmo <skmodi20bce2835@gmail.com>
2022-09-07 12:35:31 +0530

    further development commit

--- app/src/main/java/com/example/vidciph/MainActivity.java
+++ app/src/main/java/com/example/vidciph/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                startActivity(new Intent(MainActivity.this, CentralActivity.class));
             }
         });
     }
