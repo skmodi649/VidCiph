39c1f045194614348103ee7c24691c9ce5112084
Surmo <skmodi20bce2835@gmail.com>
2022-09-07 22:06:23 +0530

    added menu option to central activity commit

--- app/src/main/java/com/example/vidciph/CentralActivity.java
+++ app/src/main/java/com/example/vidciph/CentralActivity.java
@@ -2,7 +2,10 @@ package com.example.vidciph;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CentralActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class CentralActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_central);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.item1){
+            startActivity(new Intent(CentralActivity.this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
