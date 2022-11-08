8b6ed4ac8b677d0d118d70b4774cae640b950f7d
Surmo <skmodi20bce2835@gmail.com>
2022-11-08 12:24:56 +0530

    commit

---
+++ app/src/main/java/com/example/vidciph/UploadVideo.java
@@ -0,0 +1,4 @@
+package com.example.vidciph;
+
+public class UploadVideo {
+}
--- app/src/main/java/com/example/vidciph/Video_Select_EncDec.java
+++ app/src/main/java/com/example/vidciph/Video_Select_EncDec.java
@@ -1,50 +1,30 @@
 package com.example.vidciph;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.provider.MediaStore;
-import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-public class Video_Select_EncDec extends AppCompatActivity implements View.OnClickListener {
 
-    private Button buttonChoose;
-    private Button buttonUpload;
-    private TextView textView;
-    private TextView textViewResponse;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-    private static final int SELECT_VIDEO = 3;
+import java.security.NoSuchAlgorithmException;
 
-    private String selectedPath;
-
+public class Video_Select_EncDec extends AppCompatActivity {
+    AESEncDec encrypter;
+    TextView path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_select_encdec);
-
-        buttonChoose = (Button) findViewById(R.id.buttonChoose);
-        buttonUpload = (Button) findViewById(R.id.buttonUpload);
-
-        textView = (TextView) findViewById(R.id.textView);
-        textViewResponse = (TextView) findViewById(R.id.textViewResponse);
-
-        buttonChoose.setOnClickListener(this);
-        buttonUpload.setOnClickListener(this);
+        path = findViewById(R.id.pathv);
+        path.setText("");
     }
-
     ActivityResultLauncher<Intent> sActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
@@ -56,7 +36,7 @@ public class Video_Select_EncDec extends AppCompatActivity implements View.OnCli
 
                         String selectedVideoPath = String.valueOf(uri);
                         if (selectedVideoPath != null) {
-                            textViewResponse.setText(getPath(uri));
+                            path.setText(selectedVideoPath);
                         }
                     }
                 }
@@ -70,61 +50,49 @@ public class Video_Select_EncDec extends AppCompatActivity implements View.OnCli
 
     }
 
-    public String getPath(Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        String document_id = cursor.getString(0);
-        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
-        cursor.close();
-
-        cursor = getContentResolver().query(
-                android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
-        cursor.moveToFirst();
-        String path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA));
-        cursor.close();
-
-        return path;
+    public void AESEncDec(View view){
+        if(path.getText().length() == 0){
+            Toast.makeText(Video_Select_EncDec.this,"Please select a video!", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(Video_Select_EncDec.this, "Video Encrypted! Check Movies Folder!", Toast.LENGTH_LONG).show();
+        }
     }
 
-    private void uploadVideo() {
-        class UploadVideo extends AsyncTask<Void, Void, String> {
+    // performing the AES Encryption Decryption operation
 
-            ProgressDialog uploading;
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                uploading = ProgressDialog.show(Video_Select_EncDec.this, "Uploading File", "Please wait...", false, false);
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-                uploading.dismiss();
-                textViewResponse.setText(Html.fromHtml("<b>Uploaded at <a href='" + s + "'>" + s + "</a></b>"));
-                textViewResponse.setMovementMethod(LinkMovementMethod.getInstance());
-            }
-
-            @Override
-            protected String doInBackground(Void... params) {
-                Upload u = new Upload();
-                String msg = u.uploadVideo(selectedPath);
-                return msg;
-            }
-        }
-        UploadVideo uv = new UploadVideo();
-        uv.execute();
+    private void init() throws NoSuchAlgorithmException {
+        encrypter =new AESEncDec();
     }
 
 
-    @Override
-    public void onClick(View v) {
-        if (v == buttonChoose) {
-            openFileDialog(v);
+
+
+
+
+
+
+
+    //this function returns null when using IO file manager
+   /* public static String getPath(Context context, Uri uri) throws URISyntaxException {
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = { "_data" };
+            Cursor cursor = null;
+
+            try {
+                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow("_data");
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                // Eat it
+            }
         }
-        if (v == buttonUpload) {
-            uploadVideo();
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
         }
-    }
+
+        return null;
+    } */
 }
